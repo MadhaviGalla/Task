6b95a02ecb7029c598fb5b453f0a8dc83d8fcd94
MadhaviGalla <madhavi.galla@kanerika.com>
2022-12-07 12:00:24 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,46 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MainComponent } from './main/main.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+import { SalesByMonthComponent } from './sales-by-month/sales-by-month.component';
+import { SalesByCategoryComponent } from './sales-by-category/sales-by-category.component';
+import { LastFewTransactionsComponent } from './last-few-transactions/last-few-transactions.component';
+import { TopThreeProductsComponent } from './top-three-products/top-three-products.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TopWidgetsComponent } from './top-widgets/top-widgets.component';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as more from 'highcharts/highcharts-more.src';
+import * as exporting from 'highcharts/modules/exporting.src';
+import { TestComponent } from './test/test.component';
+import {InputTextModule} from 'primeng/inputtext';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SideNavComponent,
+    MainComponent,
+    WidgetsComponent,
+    SalesByMonthComponent,
+    SalesByCategoryComponent,
+    LastFewTransactionsComponent,
+    TopThreeProductsComponent,
+    TopWidgetsComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    ChartModule,InputTextModule
   ],
-  providers: [],
+  providers: [
+    {provide: HIGHCHARTS_MODULES, useFactory: () => [ more, exporting ]}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
